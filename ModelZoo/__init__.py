c90da86a454ff7f0ef9f52c7e7c6d39b96874168
Jixuying <jxy97@mail.ustc.edu.cn>
2021-04-15 20:29:55 +0800

    Update __init__.py

--- ModelZoo/__init__.py
+++ ModelZoo/__init__.py
@@ -16,7 +16,8 @@ NN_LIST = [
     'SRCNNpara_1218_rx_1sir_n10_complex_channelnet',
     'DNCNNpara_1218_rx_1sir_5_real_channelnet',
     'DNCNNpara_1218_rx_1sir_5_complex_channelnet',
-    'SRCNNpara_1218_rx_1sir_5_complex_channelnet2'
+    'SRCNNpara_1218_rx_1sir_5_complex_channelnet2',
+    'SRCNNpara_1218_rx_1sir_5_complex_channelnet3'
 ]
 
 
@@ -56,6 +57,9 @@ MODEL_LIST = {
     },
     'SRCNNpara_1218_rx_1sir_5_complex_channelnet2': {
         'Base': 'SRCNNpara_1218_rx_1sir_5_complex_channelnet2.pth',
+    },
+    'SRCNNpara_1218_rx_1sir_5_complex_channelnet3': {
+        'Base': 'SRCNNpara_1218_rx_1sir_5_complex_channelnet3.pth',
     }
 }
 
@@ -132,6 +136,10 @@ def get_model(model_name, factor=4, num_channels=3):
             net = DNCNN_ComplexNet()
             
         elif model_name == 'SRCNNpara_1218_rx_1sir_5_complex_channelnet2':
+            from .NN.SRCNN_complex import SRCNN_ComplexNet
+            net = SRCNN_ComplexNet()
+            
+        elif model_name == 'SRCNNpara_1218_rx_1sir_5_complex_channelnet3':
             from .NN.SRCNN_complex import SRCNN_ComplexNet
             net = SRCNN_ComplexNet()
 
